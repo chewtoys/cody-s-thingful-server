1fd75a5209be712b0142fcc6fdc70d5d91be84cb
Cody Gillette <cgillette1312@gmail.com>
2019-05-14 20:25:12 -0700

    finished

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,8 +1,8 @@
 'use strict';
-const AuthService = require('../auth/auth-service');
-
+const bcrypt = require('bcryptjs');
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
+
   let basicToken;
   if (!authToken.toLowerCase().startsWith('basic ')) {
     return res.status(401).json({ error: 'Missing basic token' });
@@ -10,24 +10,36 @@ function requireAuth(req, res, next) {
     basicToken = authToken.slice('basic '.length, authToken.length);
   }
 
-  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken);
+  const [tokenUserName, tokenPassword] = Buffer
+    .from(basicToken, 'base64')
+    .toString()
+    .split(':');
+
   if (!tokenUserName || !tokenPassword) {
     return res.status(401).json({ error: 'Unauthorized request' });
   }
-  AuthService.getUserWithUserName(
-    req.app.get('db'),
-    tokenUserName
-  )
+
+  req.app.get('db')('thingful_users')
+    .where({ user_name: tokenUserName })
+    .first()
     .then(user => {
-      if (!user || user.password !== tokenPassword) {
+      if (!user) {
         return res.status(401).json({ error: 'Unauthorized request' });
       }
-      req.user = user;
-      next();
+
+      return bcrypt.compare(tokenPassword, user.password)
+        .then(passwordsMatch => {
+          if (!passwordsMatch) {
+            return res.status(401).json({ error: 'Unauthorized request' });
+          }
+
+          req.user = user;
+          next();
+        });
     })
     .catch(next);
 }
 
 module.exports = {
-  requireAuth
+  requireAuth,
 };
